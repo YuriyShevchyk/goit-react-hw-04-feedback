95c0a73a65c6c8bdf7f03bd92e9e340aa0483a2c
YuriyShevchyk <97519385+YuriyShevchyk@users.noreply.github.com>
2022-12-21 02:15:51 +0200

    App bug fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,9 +8,9 @@ export default function App()  {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  };
+  
 
-  handleClick = event => {
+  const handleClick = event => {
     const key = event.target.name;
     switch (key) {
       case 'good':
@@ -24,11 +24,11 @@ export default function App()  {
     }
   };
 
-  countTotalFeedback=()=> {
+  const countTotalFeedback=()=> {
     return bad + good + neutral;
   };
 
-  countPositiveFeedbackPercentage=()=> {
+  const countPositiveFeedbackPercentage=()=> {
     const totalRates = countTotalFeedback();
     return good > 0 ? Math.round((good / totalRates) * 100) : 0;
   };
@@ -36,7 +36,7 @@ export default function App()  {
   const totalRates = countTotalFeedback();
 
   
-    return (
+  return (
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
@@ -60,4 +60,5 @@ export default function App()  {
         </Section>
       </>
     );
+};
   
